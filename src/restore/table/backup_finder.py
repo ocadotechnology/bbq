d209b78ca52d861a595f90cc38d7119fa5c25243
Przemyslaw Jasinski <p.jasinski@ocado.com>
2018-06-20 16:09:10 +0200

    YACHT-753: New restore table handler

---
+++ src/restore/table/backup_finder.py
@@ -0,0 +1,38 @@
+import logging
+from datetime import datetime
+
+from src.backup.datastore.Table import Table
+
+
+class TableNotFoundException(Exception):
+    pass
+
+
+class BackupNotFoundException(Exception):
+    pass
+
+
+class BackupFinder(object):
+
+    @classmethod
+    def for_table(cls, table_reference, not_newer_than=datetime.utcnow()):
+        table_entity = cls.__get_table_entity(table_reference)
+        backup = table_entity.last_backup_not_newer_than(not_newer_than)
+        if backup is None:
+            raise BackupNotFoundException(
+                'Backup not found for {} before {}'.format(
+                    table_reference, not_newer_than))
+        logging.info("backup: %s", backup)
+        return backup
+
+    @staticmethod
+    def __get_table_entity(table_reference):
+        table = Table.get_table(table_reference.project_id,
+                                table_reference.dataset_id,
+                                table_reference.table_id,
+                                table_reference.partition_id)
+        if table is None:
+            raise TableNotFoundException(
+                'Table not found in datastore: {}'.format(table_reference))
+        logging.info("Datastore table: %s", table)
+        return table
