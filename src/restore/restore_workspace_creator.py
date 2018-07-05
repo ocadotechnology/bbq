951be25a451da9f345e3b64f205755e2d43d0c27
Jaroslaw Drabek <jaroslaw.drabek@ocado.com>
2018-07-05 13:15:08 +0200

    YACHT-910: supporting custom partitions in restoration

--- src/restore/restore_workspace_creator.py
+++ src/restore/restore_workspace_creator.py
@@ -14,7 +14,7 @@ class RestoreWorkspaceCreator(object):
             target_table_reference)
 
         self.__create_empty_partitioned_table_if_not_exists(
-            target_table_reference)
+            source_table_reference, target_table_reference)
 
     def __create_target_dataset_if_not_exists(self, source_table_reference,
                                               target_table_reference):
@@ -39,9 +39,11 @@ class RestoreWorkspaceCreator(object):
             table_expiration
         )
 
-    def __create_empty_partitioned_table_if_not_exists(self, target_table_reference):
-        if target_table_reference.is_partition() and not BigQueryTableMetadata.get_table_by_reference_cached(target_table_reference).table_exists():
-            self.BQ.create_empty_partitioned_table(target_table_reference)
+    def __create_empty_partitioned_table_if_not_exists(self, source_table_reference, target_table_reference):
+        target_table_metadata = BigQueryTableMetadata.get_table_by_reference_cached(target_table_reference)
+        if target_table_reference.is_partition() and not target_table_metadata.table_exists():
+            source_table_metadata = BigQueryTableMetadata.get_table_by_reference_cached(source_table_reference)
+            source_table_metadata.create_the_same_empty_table(target_table_reference)
 
     @staticmethod
     def create_default_target_dataset_id(project_id, dataset_id):
