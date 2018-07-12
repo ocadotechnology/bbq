b0ce2366032fdca81362a272e44c5a506c262228
Przemyslaw Jasinski <p.jasinski@ocado.com>
2018-07-12 10:05:53 +0200

    YACHT-584: Refactor

--- src/datastore_export/load_datastore_backups_to_big_query_service.py
+++ src/datastore_export/load_datastore_backups_to_big_query_service.py
@@ -78,17 +78,10 @@ class LoadDatastoreBackupsToBigQueryService(object):
 
     def __wait_till_done(self, load_job_id):
         while True:
-            logging.info(
-                "Loading Datastore backups from GCS to BQ (jobId: %s) - "
-                "waiting %d seconds for request to end...", load_job_id, PERIOD
-            )
-            time.sleep(PERIOD)
-
             result = self.big_query.get_job(
                 project_id=configuration.backup_project_id,
                 job_id=load_job_id
             )
-
             if 'errors' in result['status']:
                 raise LoadDatastoreBackupsToBigQueryException(
                     "Export from GCS to BQ failed, job id: {}".format(
@@ -97,4 +90,10 @@ class LoadDatastoreBackupsToBigQueryService(object):
             if result['status']['state'] == 'DONE':
                 logging.info("Export from GCS to BQ finished successfully.")
                 return
-            logging.info("Export from GCS to BQ still in progress ...")
+
+            logging.info(
+                "Export from GCS to BQ still in progress... JobId: %s. "
+                "Waiting %d seconds to check the results again.",
+                load_job_id, PERIOD
+            )
+            time.sleep(PERIOD)
