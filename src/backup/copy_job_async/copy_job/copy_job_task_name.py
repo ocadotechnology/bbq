951cd8c49799a94fb65182fdcdb6d35d738a0df8
Przemyslaw Jasinski <p.jasinski@ocado.com>
2018-08-02 16:05:49 +0200

    YACHT-948: Handled creation of copy job task name if too long

--- src/backup/copy_job_async/copy_job/copy_job_task_name.py
+++ src/backup/copy_job_async/copy_job/copy_job_task_name.py
@@ -1,6 +1,11 @@
 import logging
+import re
 
 from datetime import datetime
+
+
+# The task name needs to match following expression
+EXPRESSION = re.compile("^[a-zA-Z0-9_-]{1,500}$")
 
 
 class CopyJobTaskName(object):
@@ -10,7 +15,7 @@ class CopyJobTaskName(object):
 
     def create(self):
         logging.info("INFO:  %s", self.__copy_job_request)
-        return '_'.join([
+        task_name = '_'.join([
             datetime.utcnow().strftime("%Y-%m-%d"),
             str(self.__copy_job_request.source_big_query_table),
             str(self.__copy_job_request.retry_count),
@@ -19,4 +24,4 @@ class CopyJobTaskName(object):
             .replace('$', '_')\
             .replace('.', '_')\
             .replace(':', '_')
-# The task name needs to match following expression: "^[a-zA-Z0-9_-]{1,500}$"
+        return task_name if EXPRESSION.match(task_name) else None
