b2d6f938963083040a8f7f30f4d8c965ad790526
Jaroslaw Drabek <jaroslaw.drabek@ocado.com>
2018-08-22 15:46:01 +0200

    YACHT-962: slo 4 views as a code

--- terraform/SLI_4_days_views.tf
+++ terraform/SLI_4_days_views.tf
@@ -1,12 +1,12 @@
-resource "google_bigquery_table" "census_data_5_days_ago_view" {
+resource "google_bigquery_table" "census_data_4_days_ago_view" {
   project = "${var.slos_views_destination_project}"
   dataset_id = "SLO_views_legacy"
-  table_id = "census_data_5_days_ago"
+  table_id = "census_data_4_days_ago"
 
   view {
     query = <<EOF
             #legacySQL
-              -- Shows all tables and partitions seen by census 5 days ago
+              -- Shows all tables and partitions seen by census 4 days ago
             SELECT * FROM (
               SELECT projectId, datasetId, tableId, partitionId, creationTime, lastModifiedTime
               FROM (
@@ -16,7 +16,7 @@ resource "google_bigquery_table" "census_data_5_days_ago_view" {
                 FROM
                   [${var.census_project}.bigquery.table_metadata_v1_0]
                 WHERE
-                  _PARTITIONTIME BETWEEN TIMESTAMP(DATE_ADD(CURRENT_DATE(), -8, "DAY")) AND TIMESTAMP(DATE_ADD(CURRENT_DATE(), -5, "DAY"))
+                  _PARTITIONTIME BETWEEN TIMESTAMP(DATE_ADD(CURRENT_DATE(), -7, "DAY")) AND TIMESTAMP(DATE_ADD(CURRENT_DATE(), -4, "DAY"))
                   AND timePartitioning.type IS NULL AND type='TABLE'
               )
               WHERE rownum = 1
@@ -29,7 +29,7 @@ resource "google_bigquery_table" "census_data_5_days_ago_view" {
                   FROM
                     [${var.census_project}.bigquery.partition_metadata_v1_0]
                   WHERE
-                    _PARTITIONTIME BETWEEN TIMESTAMP(DATE_ADD(CURRENT_DATE(), -8, "DAY")) AND TIMESTAMP(DATE_ADD(CURRENT_DATE(), -5, "DAY"))
+                    _PARTITIONTIME BETWEEN TIMESTAMP(DATE_ADD(CURRENT_DATE(), -7, "DAY")) AND TIMESTAMP(DATE_ADD(CURRENT_DATE(), -4, "DAY"))
               )
               WHERE rownum = 1
             )
@@ -41,10 +41,10 @@ resource "google_bigquery_table" "census_data_5_days_ago_view" {
 }
 
 
-resource "google_bigquery_table" "SLI_5_days_view" {
+resource "google_bigquery_table" "SLI_4_days_view" {
   project = "${var.slos_views_destination_project}"
   dataset_id = "SLO_views_legacy"
-  table_id = "SLI_5_days"
+  table_id = "SLI_4_days"
 
   view {
     query = <<EOF
@@ -59,7 +59,7 @@ resource "google_bigquery_table" "SLI_5_days_view" {
             IFNULL(last_backups.backup_created, MSEC_TO_TIMESTAMP(0)) as backup_created,
             IFNULL(last_backups.backup_last_modified, MSEC_TO_TIMESTAMP(0)) as backup_last_modified
           FROM
-            [${var.slos_views_destination_project}.SLO_views_legacy.census_data_5_days_ago] AS census
+            [${var.slos_views_destination_project}.SLO_views_legacy.census_data_4_days_ago] AS census
           LEFT JOIN (
             SELECT
               backup_created, backup_last_modified, source_project_id, source_dataset_id, source_table_id, source_partition_id
@@ -73,11 +73,11 @@ resource "google_bigquery_table" "SLI_5_days_view" {
             census.partitionId=last_backups.source_partition_id
           WHERE
             projectId != "${var.bbq_project}"
-            AND backup_created < TIMESTAMP(DATE_ADD(CURRENT_TIMESTAMP(), -5 , "DAY"))
+            AND backup_created < TIMESTAMP(DATE_ADD(CURRENT_TIMESTAMP(), -4 , "DAY"))
             AND backup_last_modified < lastModifiedTime
         EOF
     use_legacy_sql = true
   }
 
-  depends_on = ["google_bigquery_table.census_data_5_days_ago_view", "google_bigquery_table.last_available_backup_for_every_table_entity_view"]
+  depends_on = ["google_bigquery_table.census_data_4_days_ago_view", "google_bigquery_table.last_available_backup_for_every_table_entity_view"]
 }
