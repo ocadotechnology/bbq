3611943b7deed365189d32d23698da848910ce94
Jaroslaw Drabek <jaroslaw.drabek@ocado.com>
2018-08-22 13:51:01 +0200

    YACHT-962: slo 7 views as a code

--- terraform/slo_views.tf
+++ terraform/slo_views.tf
@@ -45,3 +45,45 @@ resource "google_bigquery_table" "census_data_7_days_ago_view" {
 
   depends_on = ["google_bigquery_dataset.SLO_views_legacy_dataset"]
 }
+
+
+resource "google_bigquery_table" "SLI_7_days_view" {
+  project = "${var.bbq_project}"
+  dataset_id = "SLO_views_legacy"
+  table_id = "SLI_7_days"
+
+  view {
+    query = <<EOF
+            #legacySQL
+            SELECT
+              census.projectId as projectId,
+              census.datasetId as datasetId,
+              census.tableId as tableId,
+              census.partitionId as partitionId,
+              census.creationTime as creationTime,
+              census.lastModifiedTime as lastModifiedTime,
+              IFNULL(last_backups.backup_created, MSEC_TO_TIMESTAMP(0)) as backup_created,
+              IFNULL(last_backups.backup_last_modified, MSEC_TO_TIMESTAMP(0)) as backup_last_modified
+            FROM
+              [${var.bbq_project}.SLO_views_legacy.census_data_7_days_ago] AS census
+            LEFT JOIN (
+              SELECT
+                backup_created, backup_last_modified, source_project_id, source_dataset_id, source_table_id, source_partition_id
+              FROM
+                [${var.datastore_export_project}:datastore_export_views_legacy.last_available_backup_for_every_table_entity]
+            ) AS last_backups
+            ON
+              census.projectId=last_backups.source_project_id AND
+              census.datasetId=last_backups.source_dataset_id AND
+              census.tableId=last_backups.source_table_id AND
+              census.partitionId=last_backups.source_partition_id
+            WHERE
+              projectId != "${var.bbq_project}"
+              AND backup_created < TIMESTAMP(DATE_ADD(CURRENT_TIMESTAMP(), -7 , "DAY"))
+              AND backup_last_modified < lastModifiedTime
+        EOF
+    use_legacy_sql = true
+  }
+
+  depends_on = ["google_bigquery_table.census_data_7_days_ago_view", "google_bigquery_table.last_available_backup_for_every_table_entity_view"]
+}
