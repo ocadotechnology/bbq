6c280e6b34d015b9da307eb8b18d9b346e374b55
Jaroslaw Drabek <jaroslaw.drabek@ocado.com>
2018-08-22 11:50:30 +0200

    YACHT-962: first view in terraform - census_data_7_days_ago

---
+++ terraform/slo_views.tf
@@ -0,0 +1,54 @@
+variable "bbq_project" {}
+variable "census_project" {}
+
+provider "google" {
+  version = "1.16"
+}
+
+resource "google_bigquery_dataset" "SLO_views_legacy_dataset" {
+  dataset_id = "SLO_views_legacy"
+  project = "${var.bbq_project}"
+  location = "EU"
+}
+
+resource "google_bigquery_table" "census_data_7_days_ago_view" {
+  project = "${var.bbq_project}"
+  dataset_id = "SLO_views_legacy"
+  table_id = "census_data_7_days_ago"
+
+  view {
+    query = <<EOF
+          #legacySQL
+            -- Shows all tables and partitions seen by census 7 days ago
+          SELECT * FROM (
+            SELECT projectId, datasetId, tableId, partitionId, creationTime, lastModifiedTime
+            FROM (
+              SELECT
+                projectId, datasetId, tableId, creationTime, lastModifiedTime, 'null' AS partitionId,
+                ROW_NUMBER() OVER (PARTITION BY projectId, datasetId, tableId ORDER BY snapshotTime DESC) AS rownum
+              FROM
+                [${var.census_project}.bigquery.table_metadata_v1_0]
+              WHERE
+                _PARTITIONTIME BETWEEN TIMESTAMP(DATE_ADD(CURRENT_DATE(), -10, "DAY")) AND TIMESTAMP(DATE_ADD(CURRENT_DATE(), -7, "DAY"))
+                AND timePartitioning.type IS NULL AND type='TABLE'
+            )
+            WHERE rownum = 1
+          ), (
+            SELECT projectId, datasetId, tableId, partitionId, creationTime, lastModifiedTime
+            FROM (
+              SELECT
+                projectId, datasetId, tableId, partitionId, creationTime, lastModifiedTime,
+                  ROW_NUMBER() OVER (PARTITION BY projectId, datasetId, tableId, partitionId ORDER BY snapshotTime DESC) AS rownum
+                FROM
+                  [${var.census_project}.bigquery.partition_metadata_v1_0]
+                WHERE
+                  _PARTITIONTIME BETWEEN TIMESTAMP(DATE_ADD(CURRENT_DATE(), -10, "DAY")) AND TIMESTAMP(DATE_ADD(CURRENT_DATE(), -7, "DAY"))
+            )
+            WHERE rownum = 1
+          )
+        EOF
+    use_legacy_sql = true
+  }
+
+  depends_on = ["google_bigquery_dataset.SLO_views_legacy_dataset"]
+}
