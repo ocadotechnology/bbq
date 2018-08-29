51d759f81a5d6ece34a44b7aabd7f3ad3e3a5533
Jaroslaw Drabek <jaroslaw.drabek@ocado.com>
2018-08-29 11:46:28 +0200

    YACHT-962: excluding variable var.SLO_views_legacy

--- terraform/SLI_5_days_views.tf
+++ terraform/SLI_5_days_views.tf
@@ -1,6 +1,6 @@
 resource "google_bigquery_table" "census_data_5_days_ago_view" {
   project = "${var.slos_views_destination_project}"
-  dataset_id = "SLO_views_legacy"
+  dataset_id = "${var.SLO_views_legacy}"
   table_id = "census_data_5_days_ago"
 
   view {
@@ -43,7 +43,7 @@ resource "google_bigquery_table" "census_data_5_days_ago_view" {
 
 resource "google_bigquery_table" "SLI_5_days_view" {
   project = "${var.slos_views_destination_project}"
-  dataset_id = "SLO_views_legacy"
+  dataset_id = "${var.SLO_views_legacy}"
   table_id = "SLI_5_days"
 
   view {
@@ -59,7 +59,7 @@ resource "google_bigquery_table" "SLI_5_days_view" {
             IFNULL(last_backups.backup_created, MSEC_TO_TIMESTAMP(0)) as backup_created,
             IFNULL(last_backups.backup_last_modified, MSEC_TO_TIMESTAMP(0)) as backup_last_modified
           FROM
-            [${var.slos_views_destination_project}.SLO_views_legacy.census_data_5_days_ago] AS census
+            [${var.slos_views_destination_project}.${var.SLO_views_legacy}.census_data_5_days_ago] AS census
           LEFT JOIN (
             SELECT
               backup_created, backup_last_modified, source_project_id, source_dataset_id, source_table_id, source_partition_id
--- terraform/SLO_views_legacy_dataset.tf
+++ terraform/SLO_views_legacy_dataset.tf
@@ -1,5 +1,5 @@
 resource "google_bigquery_dataset" "SLO_views_legacy_dataset" {
-  dataset_id = "SLO_views_legacy"
+  dataset_id = "${var.SLO_views_legacy}"
   project = "${var.slos_views_destination_project}"
   location = "EU"
 }
