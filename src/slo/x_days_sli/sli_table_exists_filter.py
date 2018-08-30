ed8961aa7c1303075b210af970bea7ff6e1c5a46
Marcin Kołda <marcin.kolda@ocado.com>
2018-08-30 11:27:16 +0200

    YACHT-964: SLI filtering refactoring

--- src/slo/x_days_sli/sli_table_exists_filter.py
+++ src/slo/x_days_sli/sli_table_exists_filter.py
@@ -2,44 +2,44 @@ import logging
 
 from apiclient.errors import HttpError
 
-from src.commons.big_query.big_query import BigQuery
-from src.commons.table_reference import TableReference
-
 
 class SLITableExistsFilter(object):
 
-    def __init__(self):
-        self.big_query = BigQuery()
+    def __init__(self, big_query):
+        self.big_query = big_query
 
-    def exists(self, table):
+    def exists(self, table_reference):
         try:
-            table_dict = self.big_query.get_table(project_id=table['projectId'],
-                                                  dataset_id=table['datasetId'],
-                                                  table_id=table['tableId'])
-            if table_dict:
-                partition_id = table['partitionId']
-                if partition_id:
-                    return self.__is_partition_exists(
-                        project_id=table['projectId'],
-                        dataset_id=table['datasetId'],
-                        table_id=table['tableId'],
-                        partition_id=partition_id)
+            table = self.big_query.get_table(
+                project_id=table_reference.project_id,
+                dataset_id=table_reference.dataset_id,
+                table_id=table_reference.table_id)
+
+            if not table:
+                logging.info("Table doesn't exist anymore: %s", table_reference)
+                return False
+
+            if not table_reference.is_partition():
                 return True
+
+            if self.__is_partition_exists(table_reference):
+                return True
+
+            logging.info("Partition doesn't exist anymore: %s", table_reference)
+            return False
+
         except HttpError as ex:
             if ex.resp.status == 403:
-                logging.warning(
-                    "Application has no access to '%s'",
-                    TableReference(table['projectId'], table['datasetId'],
-                                   table['tableId'])
-                )
+                logging.warning("Application has no access to '%s'",
+                                table_reference)
                 return False
-        logging.info("Table doesn't exist anymore: %s", table)
-        return False
+            raise ex
 
-    def __is_partition_exists(self, project_id, dataset_id, table_id,
-        partition_id):
+    def __is_partition_exists(self, table_reference):
         partitions = self.big_query.list_table_partitions(
-            project_id=project_id, dataset_id=dataset_id, table_id=table_id)
+            project_id=table_reference.project_id,
+            dataset_id=table_reference.dataset_id,
+            table_id=table_reference.table_id)
 
-        return partition_id in [partition['partitionId']
-                                for partition in partitions]
+        return table_reference.partition_id in [partition['partitionId']
+                                                for partition in partitions]
