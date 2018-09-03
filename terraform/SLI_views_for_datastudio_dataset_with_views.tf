68e6b8122708f46a6bbee55e4fb138ae5f2c5967
Jaroslaw Drabek <jaroslaw.drabek@ocado.com>
2018-09-03 17:15:08 +0200

    NON-JIRA: project-bbq SLI views have label bbq_metadata

--- terraform/SLI_views_for_datastudio_dataset_with_views.tf
+++ terraform/SLI_views_for_datastudio_dataset_with_views.tf
@@ -2,6 +2,8 @@ resource "google_bigquery_dataset" "SLI_views_for_datastudio_legacy_dataset" {
   dataset_id = "SLI_views_for_datastudio"
   project = "${var.SLI_views_destination_project}"
   location = "${var.SLI_views_location}"
+
+  labels {"bbq_metadata"=""}
 }
 
 resource "google_bigquery_table" "SLI_X_days_view" {
--- terraform/SLI_views_legacy_dataset.tf
+++ terraform/SLI_views_legacy_dataset.tf
@@ -2,4 +2,6 @@ resource "google_bigquery_dataset" "SLI_views_legacy_dataset" {
   dataset_id = "${var.SLI_views_legacy_dataset}"
   project = "${var.SLI_views_destination_project}"
   location = "${var.SLI_views_location}"
+
+  labels {"bbq_metadata"=""}
 }
