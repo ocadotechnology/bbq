efbf3e83d7dccf15e4333c45a0af7ec9e91cc284
Jaroslaw Drabek <jaroslaw.drabek@ocado.com>
2018-09-03 14:58:50 +0200

    YACHT-965: corrections after review: final changes

--- terraform/SLI_views_legacy_dataset.tf
+++ terraform/SLI_views_legacy_dataset.tf
@@ -1,4 +1,4 @@
-resource "google_bigquery_dataset" "SLI_views_legacy_dataset_dataset" {
+resource "google_bigquery_dataset" "SLI_views_legacy_dataset" {
   dataset_id = "${var.SLI_views_legacy_dataset}"
   project = "${var.SLI_views_destination_project}"
   location = "${var.SLI_views_location}"
