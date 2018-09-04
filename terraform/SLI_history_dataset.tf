9d721413f177a32367e50e4d2cdb5c25c50f3e55
Jaroslaw Drabek <jaroslaw.drabek@ocado.com>
2018-09-04 12:54:25 +0200

    YACHT-966: views for datastudio update

--- terraform/SLI_history_dataset.tf
+++ terraform/SLI_history_dataset.tf
@@ -5,3 +5,43 @@ resource "google_bigquery_dataset" "SLI_history_dataset" {
 
   labels {"bbq_metadata"=""}
 }
+
+resource "google_bigquery_table" "SLI_backup_creation_latency" {
+  project = "${local.SLI_views_destination_project}"
+  dataset_id = "${var.SLI_history_dataset}"
+  table_id = "SLI_backup_creation_latency"
+
+  time_partitioning {
+    type = "DAY"
+  }
+
+  schema= "${file("SLI_filtered_table_schema.json")}"
+
+  depends_on = ["google_bigquery_dataset.SLI_history_dataset"]
+}
+
+resource "google_bigquery_table" "SLI_backup_creation_latency_view" {
+  project = "${local.SLI_views_destination_project}"
+  dataset_id = "${var.SLI_history_dataset}"
+  table_id = "SLI_backup_creation_latency_view"
+
+  view {
+    query = "SELECT * FROM [project-bbq:SLI_history.SLI_backup_creation_latency]"
+    use_legacy_sql = true
+  }
+
+  depends_on = ["google_bigquery_table.SLI_backup_creation_latency"]
+}
+
+resource "google_bigquery_table" "SLI_backup_creation_latency_by_count_view" {
+  project = "${local.SLI_views_destination_project}"
+  dataset_id = "${var.SLI_history_dataset}"
+  table_id = "SLI_backup_creation_latency_by_count_view"
+
+  view {
+    query = "SELECT xDays days, count(*) as count FROM [${var.SLI_history_dataset}.SLI_backup_creation_latency_view] GROUP BY xDays"
+    use_legacy_sql = true
+  }
+
+  depends_on = ["google_bigquery_table.SLI_backup_creation_latency_view"]
+}
