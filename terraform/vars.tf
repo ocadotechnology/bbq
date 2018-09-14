9afac9263fea42f6d97fb2c2b561dd05c1fe2bd3
Mateusz Radko <mateusz.radko@ocado.com>
2018-09-14 15:04:34 +0200

    YACHT-1022: Adding filtering restoration projects for proper views and creating variable + update documentation.

--- terraform/vars.tf
+++ terraform/vars.tf
@@ -15,6 +15,10 @@ variable "bbq_project" {
   description = "it is needed to filter out backups from SLI"
 }
 
+variable "bbq_restoration_project" {
+  description = "it is needed to filter out restored backups from SLI"
+}
+
 variable "gcp_census_project" {
   description = "project where GCP Census data resides. More specifically we need bigquery.table_metadata_v1_0 and bigquery.partition_metadata_v1_0 table from that project"
 }
