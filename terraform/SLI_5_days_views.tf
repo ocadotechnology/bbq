22044892f9f0e6d429ce0eade43ad40866ae5564
Mateusz Radko <mateusz.radko@ocado.com>
2018-09-17 13:47:36 +0200

    YACHT-1026: Repair of SLI query - partition_id on both sides of join are now 'null' string in case of null

--- terraform/SLI_5_days_views.tf
+++ terraform/SLI_5_days_views.tf
@@ -69,10 +69,10 @@ resource "google_bigquery_table" "SLI_5_days_view" {
           ON
             census.projectId=last_backups.source_project_id AND
             census.datasetId=last_backups.source_dataset_id AND
-            census.tableId=last_backups.source_table_id
+            census.tableId=last_backups.source_table_id AND
+            census.partitionId=last_backups.source_partition_id
           WHERE
-            (last_backups.source_partition_id IS NULL OR last_backups.source_partition_id=census.partitionId)
-            AND projectId != "${var.bbq_project}"
+            projectId != "${var.bbq_project}"
             AND projectId != "${var.bbq_restoration_project}"
             AND partitionId != "__UNPARTITIONED__"
             AND IFNULL(last_backups.backup_created, MSEC_TO_TIMESTAMP(0)) < TIMESTAMP(DATE_ADD(CURRENT_TIMESTAMP(), -5 , "DAY"))
