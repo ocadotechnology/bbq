e26cb849de396cddafb7022997dd71cba024f0c4
Mateusz Radko <mateusz.radko@ocado.com>
2018-09-24 15:28:39 +0200

    YACHT-1029: Introducing sli table recreation filter - it filters out tables which was recreated (the current creationTime from bigquery.tables.get is newest that creationTime stored in last GCP census snapshot).
    Unit tests for filters and adding more unit tests for x days sli service which examine behavior for different results returned from filters.

--- tests/slo/x_days_sli/test_x_days_sli_service.py
+++ tests/slo/x_days_sli/test_x_days_sli_service.py
@@ -4,6 +4,8 @@ from mock import patch
 
 from src.slo.x_days_sli.sli_results_streamer import SLIResultsStreamer
 from src.slo.x_days_sli.sli_table_exists_filter import SLITableExistsFilter
+from src.slo.x_days_sli.sli_table_recreation_filter import \
+    SLITableRecreationFilter
 from src.slo.x_days_sli.sli_view_querier import SLIViewQuerier
 from src.slo.x_days_sli.x_days_sli_service import XDaysSLIService
 
@@ -22,12 +24,87 @@ class TestXDaysSLIService(unittest.TestCase):
         {"projectId": "p1", "datasetId": "d1", "tableId": "t1",
          "partitionId": "part1"}
     ])
-    @patch.object(SLITableExistsFilter, 'exists',
-                  side_effect=Exception("An error"))
+    @patch.object(SLITableExistsFilter, 'exists')
     @patch.object(SLIResultsStreamer, 'stream')
-    def test_table_that_caused_exception_should_not_be_filtered_out(self,
-                                                                    stream,
-                                                                    _, _1):
+    def test_table_that_not_exist_should_be_filtered_out(self,
+        stream, exists, _):
+        # given
+        exists.return_value = False
+        # when
         XDaysSLIService(3).recalculate_sli()
+        # then
+        stream.assert_called_with([])
+
+    @patch.object(SLIViewQuerier, 'query', return_value=[
+        {"projectId": "p1", "datasetId": "d1", "tableId": "t1",
+         "partitionId": "part1"}
+    ])
+    @patch.object(SLITableRecreationFilter, 'is_recreated')
+    @patch.object(SLITableExistsFilter, 'exists')
+    @patch.object(SLIResultsStreamer, 'stream')
+    def test_table_that_exist_and_is_recreated_should_be_filtered_out(self,
+        stream, exists, is_recreated, _):
+        # given
+        exists.return_value = True
+        is_recreated.return_value = True
+        # when
+        XDaysSLIService(3).recalculate_sli()
+        # then
+        stream.assert_called_with([])
+
+    @patch.object(SLIViewQuerier, 'query', return_value=[
+        {"projectId": "p1", "datasetId": "d1", "tableId": "t1",
+         "partitionId": "part1"}
+    ])
+    @patch.object(SLITableRecreationFilter, 'is_recreated')
+    @patch.object(SLITableExistsFilter, 'exists')
+    @patch.object(SLIResultsStreamer, 'stream')
+    def test_table_that_exist_and_is_not_recreated_should_not_be_filtered_out(
+            self, stream, exists, is_recreated, _):
+        # given
+        exists.return_value = True
+        is_recreated.return_value = False
+        # when
+        XDaysSLIService(3).recalculate_sli()
+        # then
+        stream.assert_called_with([{"projectId": "p1", "datasetId": "d1",
+                                    "tableId": "t1", "partitionId": "part1"}])
+
+    @patch.object(SLIViewQuerier, 'query', return_value=[
+        {"projectId": "p1", "datasetId": "d1", "tableId": "t1",
+         "partitionId": "part1"}
+    ])
+    @patch.object(SLITableExistsFilter, 'exists')
+    @patch.object(SLIResultsStreamer, 'stream')
+    def test_table_that_caused_exception_in_exist_filter_should_not_be_filtered_out(
+            self, stream, exists, _1, ):
+
+        # given
+        exists.side_effect = Exception("An error")
+        # when
+        XDaysSLIService(3).recalculate_sli()
+
+        # then
+        stream.assert_called_with([{"projectId": "p1", "datasetId": "d1",
+                                    "tableId": "t1", "partitionId": "part1"}])
+
+    @patch.object(SLIViewQuerier, 'query', return_value=[
+        {"projectId": "p1", "datasetId": "d1", "tableId": "t1",
+         "partitionId": "part1"}
+    ])
+    @patch.object(SLITableRecreationFilter, 'is_recreated')
+    @patch.object(SLITableExistsFilter, 'exists')
+    @patch.object(SLIResultsStreamer, 'stream')
+    def test_table_that_caused_exception_in_recreation_filter_should_not_be_filtered_out(
+            self, stream, exists, is_recreated, _1):
+
+        # given
+        exists.return_value = True
+        is_recreated.side_effect = Exception("An error")
+
+        # when
+        XDaysSLIService(3).recalculate_sli()
+
+        # then
         stream.assert_called_with([{"projectId": "p1", "datasetId": "d1",
                                     "tableId": "t1", "partitionId": "part1"}])
