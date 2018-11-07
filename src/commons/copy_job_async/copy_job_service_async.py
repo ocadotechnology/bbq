5f1c275a8075779f98ea9415fd0f753f09e78126
Jaroslaw Drabek <jaroslaw.drabek@ocado.com>
2018-11-07 16:59:42 +0100

    YACHT-1092: fluent API for write/create disposition

--- src/commons/copy_job_async/copy_job_service_async.py
+++ src/commons/copy_job_async/copy_job_service_async.py
@@ -8,6 +8,8 @@ class CopyJobServiceAsync(object):
         self.__copy_job_type_id = copy_job_type_id
         self.__task_name_suffix = task_name_suffix
         self.__post_copy_action_request = None
+        self.__create_disposition = "CREATE_IF_NEEDED"
+        self.__write_disposition = "WRITE_EMPTY"
         assert self.__copy_job_type_id is not None, "copy_job_type_id needs to be assigned in constructor"
         assert self.__task_name_suffix is not None, "task_name_suffix needs to be assigned in constructor"
 
@@ -15,9 +17,15 @@ class CopyJobServiceAsync(object):
         self.__post_copy_action_request = post_copy_action_request
         return self
 
-    def copy_table(self, source_big_query_table, target_big_query_table,
-                   create_disposition="CREATE_IF_NEEDED",
-                   write_disposition="WRITE_EMPTY"):
+    def with_create_disposition(self, create_disposition):
+        self.__create_disposition = create_disposition
+        return self
+
+    def with_write_disposition(self, write_disposition):
+        self.__write_disposition = write_disposition
+        return self
+
+    def copy_table(self, source_big_query_table, target_big_query_table):
         copy_job_request = CopyJobRequest(
             task_name_suffix=self.__task_name_suffix,
             copy_job_type_id=self.__copy_job_type_id,
@@ -25,7 +33,7 @@ class CopyJobServiceAsync(object):
             target_big_query_table=target_big_query_table,
             retry_count=0,
             post_copy_action_request=self.__post_copy_action_request,
-            create_disposition=create_disposition,
-            write_disposition=write_disposition
+            create_disposition=self.__create_disposition,
+            write_disposition=self.__write_disposition
         )
         TaskCreator.create_copy_job(copy_job_request)
