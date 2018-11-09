586f3220b001e85e77038d09db535c7e9445816d
Jaroslaw Drabek <jaroslaw.drabek@ocado.com>
2018-11-09 10:12:15 +0100

    YACHT-1098: Refactor BigQuery package so it is not using table_reference. vol3

--- tests/commons/copy_job_async/test_copy_job_result.py
+++ tests/commons/copy_job_async/test_copy_job_result.py
@@ -1,5 +1,6 @@
 import unittest
 
+from src.commons.big_query.big_query_table import BigQueryTable
 from src.commons.big_query.copy_job_async.copy_job_result import CopyJobResult
 from src.commons.table_reference import TableReference
 from tests.commons.copy_job_async.result_check.job_result_example import \
@@ -24,12 +25,13 @@ class TestCopyJobResult(unittest.TestCase):
         # given
         copy_job_result = CopyJobResult(JobResultExample.DONE)
         # when
-        target_table_reference = copy_job_result.target_table_reference
+        target_bq_table = copy_job_result.target_bq_table
         # then
-        self.assertEqual(target_table_reference,
-                         TableReference(project_id="target_project_id",
-                                        dataset_id="target_dataset_id",
-                                        table_id="target_table_id"))
+        self.assertEqual(target_bq_table, BigQueryTable(
+            project_id="target_project_id",
+            dataset_id="target_dataset_id",
+            table_id="target_table_id")
+        )
 
     def test_create_disposition(self):
         # given
