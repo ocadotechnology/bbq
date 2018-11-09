85627acf1d71c1a79d12ddc97b2f54205ad78d8a
Jaroslaw Drabek <jaroslaw.drabek@ocado.com>
2018-11-09 12:49:10 +0100

    YACHT-1098: moving copy_job_async tests from commons to commons/big_query

--- tests/commons/big_query/copy_job_async/test_copy_job_result.py
+++ tests/commons/big_query/copy_job_async/test_copy_job_result.py
@@ -1,9 +1,9 @@
 import unittest
 
+from src.commons.big_query.big_query_table import BigQueryTable
 from src.commons.big_query.copy_job_async.copy_job_result import CopyJobResult
-from src.commons.table_reference import TableReference
-from tests.commons.copy_job_async.result_check.job_result_example import \
-    JobResultExample
+from tests.commons.big_query.copy_job_async.result_check.job_result_example \
+    import JobResultExample
 
 
 class TestCopyJobResult(unittest.TestCase):
@@ -12,24 +12,24 @@ class TestCopyJobResult(unittest.TestCase):
         # given
         copy_job_result = CopyJobResult(JobResultExample.DONE)
         # when
-        source_table_reference = copy_job_result.source_table_reference
+        source_bq_table = copy_job_result.source_bq_table
         # then
-        self.assertEqual(source_table_reference,
-                         TableReference(project_id="source_project_id",
+        self.assertEqual(source_bq_table,
+                         BigQueryTable(project_id="source_project_id",
                                         dataset_id="source_dataset_id",
-                                        table_id="source_table_id",
-                                        partition_id="123"))
+                                        table_id="source_table_id$123"))
 
     def test_target_table_reference(self):
         # given
         copy_job_result = CopyJobResult(JobResultExample.DONE)
         # when
-        target_table_reference = copy_job_result.target_table_reference
+        target_bq_table = copy_job_result.target_bq_table
         # then
-        self.assertEqual(target_table_reference,
-                         TableReference(project_id="target_project_id",
-                                        dataset_id="target_dataset_id",
-                                        table_id="target_table_id"))
+        self.assertEqual(target_bq_table, BigQueryTable(
+            project_id="target_project_id",
+            dataset_id="target_dataset_id",
+            table_id="target_table_id")
+        )
 
     def test_create_disposition(self):
         # given
