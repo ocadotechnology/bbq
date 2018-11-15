612f1f6f79e6e71fd59cd38790885f8821f1c462
Mateusz Radko <mateusz.radko@ocado.com>
2018-11-15 14:56:03 +0100

    YACHT-1092: Passing create/write dispostion via RestorationJob for AsyncRestoreService (as it is common for whole restore). Updating paths in app.yaml. Updating restore status to show write/create disposition.
    Passing RestorationJob as object in parameters to avoid unnecessary calls to Datastore.

--- tests/restore/test_async_restore_service.py
+++ tests/restore/test_async_restore_service.py
@@ -1,18 +1,17 @@
 import uuid
-from unittest import TestCase
 
+from apiclient.errors import HttpError
 from google.appengine.ext import testbed, ndb
 from mock import patch, call, mock
+from unittest import TestCase
 
 from src.commons.big_query.copy_job_async.post_copy_action_request \
-    import PostCopyActionRequest
+  import PostCopyActionRequest
+from src.commons.table_reference import TableReference
 from src.restore.async_batch_restore_service import AsyncBatchRestoreService
 from src.restore.datastore.restoration_job import RestorationJob
 from src.restore.datastore.restore_item import RestoreItem
 from src.restore.restore_workspace_creator import RestoreWorkspaceCreator
-from apiclient.errors import HttpError
-
-from src.commons.table_reference import TableReference
 
 HARDCODED_UUID = '123'
 
@@ -39,14 +38,17 @@ class TestAsyncRestoreService(TestCase):
 
     def test_for_single_item_should_create_post_copy_action(self):
         # given
-        RestorationJob.create(HARDCODED_UUID)
+        restoration_job_key = RestorationJob.create(
+            HARDCODED_UUID,
+            create_disposition="CREATE_IF_NEEDED",
+            write_disposition="WRITE_EMPTY")
         restore_items_tuple = self.__create_restore_items(count=1)
         restore_item = restore_items_tuple[0][0]
         source_bq = restore_items_tuple[0][1].create_big_query_table()
         target_bq = restore_items_tuple[0][2].create_big_query_table()
 
         # when
-        AsyncBatchRestoreService().restore(HARDCODED_UUID, [[restore_item]])
+        AsyncBatchRestoreService().restore(restoration_job_key, [[restore_item]])
 
         # then
         self.copy_service.assert_has_calls(
@@ -59,7 +61,9 @@ class TestAsyncRestoreService(TestCase):
                             'restoreItemKey': (
                                 restore_item.key.urlsafe())
                         })),
-                call().with_post_action().copy_table(source_bq, target_bq)
+                call().with_post_action().with_create_disposition('CREATE_IF_NEEDED'),
+                call().with_post_action().with_create_disposition().with_write_disposition('WRITE_EMPTY'),
+                call().with_post_action().with_create_disposition().with_write_disposition().copy_table(source_bq, target_bq)
             ])
 
     @patch.object(RestoreWorkspaceCreator, 'create_workspace', side_effect=
@@ -67,12 +71,12 @@ class TestAsyncRestoreService(TestCase):
     def test_failing_creating_dataset_should_update_restore_item_status(self,
         _):
         # given
-        restoration_job_key = RestorationJob.create(HARDCODED_UUID)
+        restoration_job_key = RestorationJob.create(HARDCODED_UUID, create_disposition="CREATE_IF_NEEDED", write_disposition="WRITE_EMPTY")
         restore_items_tuple = self.__create_restore_items(count=1)
         restore_item = restore_items_tuple[0][0]
 
         # when
-        AsyncBatchRestoreService().restore(HARDCODED_UUID, [[restore_item]])
+        AsyncBatchRestoreService().restore(restoration_job_key, [[restore_item]])
 
         # then
         restore_items = list(RestoreItem.query().filter(
@@ -83,7 +87,10 @@ class TestAsyncRestoreService(TestCase):
 
     def test_multiple_items_restore(self):
         # given
-        RestorationJob.create(HARDCODED_UUID)
+        restoration_job_key = RestorationJob.create(
+            HARDCODED_UUID,
+            create_disposition="CREATE_IF_NEEDED",
+            write_disposition="WRITE_EMPTY")
         restore_items_tuple = self.__create_restore_items(count=2)
         restore_item1 = restore_items_tuple[0][0]
         restore_item2 = restore_items_tuple[1][0]
@@ -92,7 +99,7 @@ class TestAsyncRestoreService(TestCase):
         source_bq2 = restore_items_tuple[1][1].create_big_query_table()
         target_bq2 = restore_items_tuple[1][2].create_big_query_table()
         # when
-        AsyncBatchRestoreService().restore(HARDCODED_UUID,
+        AsyncBatchRestoreService().restore(restoration_job_key,
                                            [[restore_items_tuple[0][0],
                                              restore_items_tuple[1][0]]])
 
@@ -105,7 +112,9 @@ class TestAsyncRestoreService(TestCase):
                              'restoreItemKey': (
                                  restore_item1.key.urlsafe())
                          })),
-                 call().with_post_action().copy_table(source_bq1, target_bq1),
+                 call().with_post_action().with_create_disposition('CREATE_IF_NEEDED'),
+                 call().with_post_action().with_create_disposition().with_write_disposition('WRITE_EMPTY'),
+                 call().with_post_action().with_create_disposition().with_write_disposition().copy_table(source_bq1, target_bq1),
                  call(copy_job_type_id='restore', task_name_suffix='123'),
                  call().with_post_action(
                      PostCopyActionRequest(
@@ -114,17 +123,19 @@ class TestAsyncRestoreService(TestCase):
                              'restoreItemKey': (
                                  restore_item2.key.urlsafe())
                          })),
-                 call().with_post_action().copy_table(source_bq2, target_bq2)
+                 call().with_post_action().with_create_disposition('CREATE_IF_NEEDED'),
+                 call().with_post_action().with_create_disposition().with_write_disposition('WRITE_EMPTY'),
+                 call().with_post_action().with_create_disposition().with_write_disposition().copy_table(source_bq2, target_bq2)
                  ]
         self.copy_service.assert_has_calls(calls)
 
     def test_that_enforcer_is_called_for_each_restore_item(self):
         # given
-        RestorationJob.create(HARDCODED_UUID)
+        restoration_job_key= RestorationJob.create(HARDCODED_UUID, create_disposition="CREATE_IF_NEEDED", write_disposition="WRITE_EMPTY")
         restore_items_tuple = self.__create_restore_items(count=2)
 
         # when
-        AsyncBatchRestoreService().restore(HARDCODED_UUID,
+        AsyncBatchRestoreService().restore(restoration_job_key,
                                            [[restore_items_tuple[0][0],
                                              restore_items_tuple[1][0]]])
 
@@ -138,10 +149,13 @@ class TestAsyncRestoreService(TestCase):
 
     def test_that_proper_entities_were_stored_in_datastore(self):
         # given
-        RestorationJob.create(HARDCODED_UUID)
+        restoration_job_key = RestorationJob.create(
+            HARDCODED_UUID,
+            create_disposition="CREATE_IF_NEEDED",
+            write_disposition="WRITE_EMPTY")
         restore_item_tuples = self.__create_restore_items(count=3)
         # when
-        AsyncBatchRestoreService().restore(HARDCODED_UUID,
+        AsyncBatchRestoreService().restore(restoration_job_key,
                                            [[restore_item_tuples[0][0],
                                              restore_item_tuples[1][0]],
                                             [restore_item_tuples[2][0]]])
