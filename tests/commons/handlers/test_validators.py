6129e1273a915c1d187f5f079760844e6f5539dc
Przemyslaw Jasinski <p.jasinski@ocado.com>
2018-11-20 13:23:27 +0100

    YACHT-1094: Added tests for validators class

--- tests/commons/handlers/test_validators.py
+++ tests/commons/handlers/test_validators.py
@@ -1,11 +1,74 @@
 import unittest
 
 from src.commons.handlers import validators
-from src.commons.handlers.validators import WrongDatasetNameException
+from src.commons.handlers.validators import WrongDatasetNameException, \
+    WrongProjectNameException, WrongCreateDispositionException, \
+    WrongWriteDispositionException
 
 
 class TestValidators(unittest.TestCase):
 
+    def test_validate_dataset_restore_params_happy_path(self):
+        validators.validate_dataset_restore_params(
+            source_project_id="source-project-id",
+            source_dataset_id="source_dataset_id",
+            target_project_id="target-project-id",
+            target_dataset_id="target_dataset_id",
+            create_disposition="CREATE_IF_NEEDED",
+            write_disposition="WRITE_APPEND"
+        )
+
+    def test_validate_dataset_restore_params_for_none_parameters(self):
+        validators.validate_dataset_restore_params(
+            source_project_id=None,
+            source_dataset_id=None,
+            target_project_id=None,
+            target_dataset_id=None,
+            create_disposition=None,
+            write_disposition=None
+        )
+
+    def test_validate_list_restore_params_happy_path(self):
+        validators.validate_list_restore_params(
+            target_project_id="target-project-id",
+            target_dataset_id="target_dataset_id",
+            create_disposition="CREATE_NEVER",
+            write_disposition="WRITE_EMPTY"
+        )
+
+    def test_validate_list_restore_params_for_none_parameters(self):
+        validators.validate_list_restore_params(
+            target_project_id=None,
+            target_dataset_id=None,
+            create_disposition=None,
+            write_disposition=None
+        )
+
+    # --------------- PROJECT ID TESTS -------------------
+    def test_valid_project_id(self):
+        validators.validate_project_id('project-id')
+
+    def test_invalid_project_with_underscore(self):
+        with self.assertRaises(WrongProjectNameException):
+            validators.validate_project_id('project_123')
+
+    def test_invalid_none_project(self):
+        with self.assertRaises(WrongProjectNameException):
+            validators.validate_project_id(None)
+
+    def test_invalid_empty_project(self):
+        with self.assertRaises(WrongProjectNameException):
+            validators.validate_project_id('')
+
+    def test_invalid_empty_project_with_spaces(self):
+        with self.assertRaises(WrongProjectNameException):
+            validators.validate_project_id('    ')
+
+    def test_invalid_project_with_slash(self):
+        with self.assertRaises(WrongProjectNameException):
+            validators.validate_project_id('project/')
+
+    # --------------- DATASET ID TESTS -------------------
     def test_valid_dataset_id(self):
         validators.validate_dataset_id('DATA_set_123')
 
@@ -29,3 +92,42 @@ class TestValidators(unittest.TestCase):
         with self.assertRaises(WrongDatasetNameException):
             validators.validate_dataset_id('data/')
 
+    # ----------- CREATE DISPOSITION TESTS ---------------
+    def test_valid_create_disposition(self):
+        validators.validate_create_disposition('CREATE_IF_NEEDED')
+
+    def test_invalid_create_disposition(self):
+        with self.assertRaises(WrongCreateDispositionException):
+            validators.validate_create_disposition('INVALID')
+
+    def test_invalid_none_create_disposition(self):
+        with self.assertRaises(WrongCreateDispositionException):
+            validators.validate_create_disposition(None)
+
+    def test_invalid_empty_create_disposition(self):
+        with self.assertRaises(WrongCreateDispositionException):
+            validators.validate_create_disposition('')
+
+    def test_invalid_empty_create_disposition_with_spaces(self):
+        with self.assertRaises(WrongCreateDispositionException):
+            validators.validate_create_disposition('    ')
+
+    # ----------- WRITE DISPOSITION TESTS ---------------
+    def test_valid_write_disposition(self):
+        validators.validate_write_disposition('WRITE_TRUNCATE')
+
+    def test_invalid_write_disposition(self):
+        with self.assertRaises(WrongWriteDispositionException):
+            validators.validate_write_disposition('INVALID')
+
+    def test_invalid_none_write_disposition(self):
+        with self.assertRaises(WrongWriteDispositionException):
+            validators.validate_write_disposition(None)
+
+    def test_invalid_empty_write_disposition(self):
+        with self.assertRaises(WrongWriteDispositionException):
+            validators.validate_write_disposition('')
+
+    def test_invalid_empty_write_disposition_with_spaces(self):
+        with self.assertRaises(WrongWriteDispositionException):
+            validators.validate_write_disposition('    ')
