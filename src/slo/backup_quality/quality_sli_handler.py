860866c1aae74cc2075e0121aacb36a4b521617a
marek.tabor <marek.tabor@ocado.com>
2018-11-27 15:44:24 +0100

    YACHT-1104: created taskqueue with separate tasks. Additional handler for those tasks. Added new sli-table-quality-violations queue to config file.

--- src/slo/backup_quality/quality_sli_handler.py
+++ src/slo/backup_quality/quality_sli_handler.py
@@ -5,11 +5,16 @@ from src.slo.backup_quality.quality_sli_service import QualitySliService
 
 
 class QualitySliHandler(webapp2.RequestHandler):
-
     def post(self):
         QualitySliService().recalculate_sli()
 
 
+class ViolationSliHandler(webapp2.RequestHandler):
+    def post(self):
+        QualitySliService().check_and_stream_violation(self.request.json['table'])
+
+
 app = webapp2.WSGIApplication([
-    ('/sli/quality', QualitySliHandler)
+    ('/sli/quality', QualitySliHandler),
+    ('/sli/quality/violation', ViolationSliHandler)
 ], debug=configuration.debug_mode)
--- tests/slo/backup_quality/test_quality_sli_service.py
+++ tests/slo/backup_quality/test_quality_sli_service.py
@@ -3,12 +3,14 @@ import unittest
 from mock import patch
 
 from src.slo.predicate.sli_table_exists_predicate import \
-  SLITableExistsPredicate
+    SLITableExistsPredicate
 from src.slo.backup_quality.quality_sli_service import QualitySliService
 from src.slo.backup_quality.predicate.sli_table_newer_modification_predicate import \
-  SLITableNewerModificationPredicate
+    SLITableNewerModificationPredicate
 from src.slo.sli_results_streamer import SLIResultsStreamer
 from src.slo.sli_view_querier import SLIViewQuerier
+from src.commons.test_utils import utils
+from google.appengine.ext import testbed
 
 
 class TestQualitySliService(unittest.TestCase):
@@ -16,10 +18,14 @@ class TestQualitySliService(unittest.TestCase):
     def setUp(self):
         patch('googleapiclient.discovery.build').start()
         patch('oauth2client.client.GoogleCredentials.get_application_default') \
-          .start()
+            .start()
+        self.testbed = testbed.Testbed()
+        self.testbed.activate()
+        self.taskqueue_stub = utils.init_testbed_queue_stub(self.testbed)
 
     def tearDown(self):
         patch.stopall()
+        self.testbed.deactivate()
 
     @patch.object(SLIViewQuerier, 'query',
                   return_value=([
@@ -38,7 +44,14 @@ class TestQualitySliService(unittest.TestCase):
         # when
         QualitySliService().recalculate_sli()
         # then
-        stream.assert_called_with([], snapshot_marker=self.__create_snapshot_marker_row(21342134324))
+        tasks = self.taskqueue_stub.get_filtered_tasks(
+            queue_names='sli-table-quality-violations')
+
+        self.assertEqual(len(tasks), 1)
+        self.assertIn('21342134324', tasks[0].payload)
+
+        stream.assert_called_once()
+        # stream.assert_called_with([], snapshot_marker=self.__create_snapshot_marker_row(21342134324))
 
     @patch.object(SLIViewQuerier, 'query',
                   return_value=([
@@ -57,40 +70,45 @@ class TestQualitySliService(unittest.TestCase):
         # when
         QualitySliService().recalculate_sli()
         # then
-        stream.assert_called_with([{"projectId": "p1", "datasetId": "d1",
-                                    "tableId": "t1", "partitionId": "part1"}],
-                                  snapshot_marker=self.__create_snapshot_marker_row(21342134324))
+        tasks = self.taskqueue_stub.get_filtered_tasks(
+            queue_names='sli-table-quality-violations')
+        self.assertEqual(len(tasks), 1)
+        # stream.assert_called_with([{"projectId": "p1", "datasetId": "d1",
+        #                             "tableId": "t1", "partitionId": "part1"}],
+        #                           snapshot_marker=self.__create_snapshot_marker_row(21342134324))
 
     @patch.object(SLIViewQuerier, 'query',
                   return_value=([
-                                  {"projectId": "p1", "datasetId": "d1",
-                                   "tableId": "t1",
-                                   "partitionId": "part1"}
+                                    {"projectId": "p1", "datasetId": "d1",
+                                     "tableId": "t1",
+                                     "partitionId": "part1"}
                                 ], 21342134324))
     @patch.object(SLITableExistsPredicate, 'exists')
     @patch.object(SLIResultsStreamer, 'stream')
     def test_table_that_not_exists_should_be_be_filtered_out(self,
-        stream, exists, _):
+                                                             stream, exists, _):
         # given
         exists.return_value = False
         # when
         QualitySliService().recalculate_sli()
         # then
-        stream.assert_called_with([],
-                                  snapshot_marker=self.__create_snapshot_marker_row(21342134324))
+        tasks = self.taskqueue_stub.get_filtered_tasks(
+            queue_names='sli-table-quality-violations')
+        self.assertEqual(len(tasks), 1)
+        # stream.assert_called_with([],
+        #                           snapshot_marker=self.__create_snapshot_marker_row(21342134324))
 
     @patch.object(SLIViewQuerier, 'query',
                   return_value=([
-                                  {"projectId": "p1", "datasetId": "d1",
-                                   "tableId": "t1",
-                                   "partitionId": "part1"}
+                                    {"projectId": "p1", "datasetId": "d1",
+                                     "tableId": "t1",
+                                     "partitionId": "part1"}
                                 ], 21342134324))
     @patch.object(SLITableExistsPredicate, 'exists')
     @patch.object(SLITableNewerModificationPredicate, 'is_modified_since_last_census_snapshot')
     @patch.object(SLIResultsStreamer, 'stream')
     def test_table_that_caused_exception_in_modification_predicate_should_not_be_filtered_out(
-        self, stream, is_modified_since_last_census_snapshot,exists, _):
-
+            self, stream, is_modified_since_last_census_snapshot, exists, _):
         # given
         exists.return_value = True
         is_modified_since_last_census_snapshot.side_effect = Exception("An error")
@@ -99,31 +117,36 @@ class TestQualitySliService(unittest.TestCase):
         QualitySliService().recalculate_sli()
 
         # then
-        stream.assert_called_with([{"projectId": "p1", "datasetId": "d1",
-                                    "tableId": "t1", "partitionId": "part1"}],
-                                  snapshot_marker=self.__create_snapshot_marker_row(21342134324))
+        tasks = self.taskqueue_stub.get_filtered_tasks(
+            queue_names='sli-table-quality-violations')
+        self.assertEqual(len(tasks), 1)
+        # stream.assert_called_with([{"projectId": "p1", "datasetId": "d1",
+        #                             "tableId": "t1", "partitionId": "part1"}],
+        #                           snapshot_marker=self.__create_snapshot_marker_row(21342134324))
 
     @patch.object(SLIViewQuerier, 'query',
-                      return_value=([
-                                      {"projectId": "p1", "datasetId": "d1",
-                                       "tableId": "t1",
-                                       "partitionId": "part1"}
-                                    ], 21342134324))
+                  return_value=([
+                                    {"projectId": "p1", "datasetId": "d1",
+                                     "tableId": "t1",
+                                     "partitionId": "part1"}
+                                ], 21342134324))
     @patch.object(SLITableExistsPredicate, 'exists')
     @patch.object(SLIResultsStreamer, 'stream')
     def test_table_that_caused_exception_in_exists_predicate_should_not_be_filtered_out(
-        self, stream, exists, _):
+            self, stream, exists, _):
+        # given
+        exists.return_value = Exception("An error")
 
-      # given
-      exists.return_value = Exception("An error")
+        # when
+        QualitySliService().recalculate_sli()
 
-      # when
-      QualitySliService().recalculate_sli()
-
-      # then
-      stream.assert_called_with([{"projectId": "p1", "datasetId": "d1",
-                                  "tableId": "t1", "partitionId": "part1"}],
-                                snapshot_marker=self.__create_snapshot_marker_row(21342134324))
+        # then
+        tasks = self.taskqueue_stub.get_filtered_tasks(
+            queue_names='sli-table-quality-violations')
+        self.assertEqual(len(tasks), 1)
+        # stream.assert_called_with([{"projectId": "p1", "datasetId": "d1",
+        #                             "tableId": "t1", "partitionId": "part1"}],
+        #                           snapshot_marker=self.__create_snapshot_marker_row(21342134324))
 
     def __create_snapshot_marker_row(self, snapshot_time):
         return {"snapshotTime": snapshot_time,
@@ -142,4 +165,3 @@ class TestQualitySliService(unittest.TestCase):
                 "numRows": 0,
                 "backupNumRows": 0
                 }
-
