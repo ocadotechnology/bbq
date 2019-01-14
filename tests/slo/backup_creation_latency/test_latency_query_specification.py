1488bc7d4d0ba0fdde8e647c9735e142dcf99ba6
marek.tabor <marek.tabor@ocado.com>
2019-01-14 09:54:46 +0100

    YACHT-1143: Update BBQ config to skip projects in SLI, by excluding projectIds during constructing query.

--- tests/slo/backup_creation_latency/test_latency_query_specification.py
+++ tests/slo/backup_creation_latency/test_latency_query_specification.py
@@ -9,8 +9,8 @@ class TestLatencyQuerySpecification(TestCase):
     @patch.object(Configuration, 'projects_to_skip', return_value=['BBQ-project-id', 'ta-testing-project', '123'],
                   new_callable=PropertyMock)
     def test_should_create_valid_query_with_skipped_projects_in_it(self, projects_to_skip):
-        projects_to_skip.return_value = ['BBQ-project-id', 'ta-testing-project', '123']
+        projects_to_skip.return_value = ['BBQ-project-id', '123']
         latency_query_spec = LatencyQuerySpecification(3)
         query = latency_query_spec.query_string()
         self.assertEqual(query,
-                         "SELECT * FROM [BBQ-project-id:SLI_backup_creation_latency_views.SLI_3_days] WHERE projectId NOT IN ('BBQ-project-id', 'ta-testing-project', '123')")
+                         "SELECT * FROM [BBQ-project-id:SLI_backup_creation_latency_views.SLI_3_days] WHERE projectId NOT IN ('BBQ-project-id', '123')")
--- tests/slo/backup_quality/test_quality_query_specification.py
+++ tests/slo/backup_quality/test_quality_query_specification.py
@@ -9,8 +9,8 @@ class TestQualityQuerySpecification(TestCase):
     @patch.object(Configuration, 'projects_to_skip', return_value=['BBQ-project-id', 'ta-testing-project', '123'],
                   new_callable=PropertyMock)
     def test_should_create_valid_query_with_skipped_projects_in_it(self, projects_to_skip):
-        projects_to_skip.return_value = ['BBQ-project-id', 'ta-testing-project', '123']
+        projects_to_skip.return_value = ['BBQ-project-id', '123']
         quality_query_spec = QualityQuerySpecification()
         query = quality_query_spec.query_string()
         self.assertEqual(query,
-                         "SELECT * FROM [BBQ-project-id:SLI_backup_quality_views.SLI_quality] WHERE projectId NOT IN ('BBQ-project-id', 'ta-testing-project', '123')")
+                         "SELECT * FROM [BBQ-project-id:SLI_backup_quality_views.SLI_quality] WHERE projectId NOT IN ('BBQ-project-id', '123')")
