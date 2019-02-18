ff3ab4ea8f723d1f30b0fd10a975712358b0b469
Przemyslaw Jasinski <p.jasinski@ocado.com>
2019-02-18 14:54:15 +0100

    YACHT-1186: Trying to fix smoke and unit tests

--- src/commons/big_query/big_query.py
+++ src/commons/big_query/big_query.py
@@ -2,7 +2,6 @@ import json
 import logging
 
 import googleapiclient.discovery
-import httplib2
 from apiclient.errors import HttpError, Error
 from oauth2client.client import GoogleCredentials
 
@@ -27,18 +26,21 @@ class DatasetNotFoundException(Exception):
 
 class BigQuery(object):
     def __init__(self):
-        self.http = self._create_http()
         self.service = googleapiclient.discovery.build(
             'bigquery',
             'v2',
-            credentials=GoogleCredentials.get_application_default(),
-            http=self.http,
+            credentials=self._create_credentials(),
+            http=self._create_http(),
             cache_discovery=self._cache_discovery()
         )
 
     @staticmethod
+    def _create_credentials():
+        return GoogleCredentials.get_application_default()
+
+    @staticmethod
     def _create_http():
-        return httplib2.Http(timeout=60)
+        return None
 
     @staticmethod
     def _cache_discovery():
