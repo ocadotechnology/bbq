07ccd5f02d785c17f9ed9be49d83e24c0f6bc79e
Marcin Kołda <marcin.kolda@ocado.com>
2019-02-19 11:39:23 +0100

    YACHT-1187: removed beta provider from TF config

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,1 +1,12 @@
-provider "google-beta" {}
+terraform {
+  required_version = "~> 0.11"
+
+  backend "gcs" {
+    bucket = {}
+    prefix = "/terraform-bbq-sli.tfstate"
+  }
+}
+
+provider "google" {
+  version = "~> 2"
+}
