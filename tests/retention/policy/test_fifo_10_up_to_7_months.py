a8415d40c4948c8abe0528674cc77d51330277f9
Mateusz Radko <mateusz.radko@ocado.com>
2019-04-12 18:05:04 +0200

    YACHT-1316: Updating retention process to avoid possible situation that creating new backup of table, which previous backup has more than 7 months, causes removing previous version of backup.
    Removing unused code. Adding units tests for all currently known situations.

--- tests/retention/policy/test_fifo_10_up_to_7_months.py
+++ tests/retention/policy/test_fifo_10_up_to_7_months.py
@@ -2,14 +2,13 @@ import unittest
 from datetime import datetime
 
 from freezegun import freeze_time
-
 from mock import patch
 
-from src.commons.big_query.big_query_table_metadata import BigQueryTableMetadata
 from src.backup.datastore.Backup import Backup
 from src.backup.datastore.Table import Table
-from src.retention.policy.fifo_10_up_to_7_months import Fifo10UpTo7Months
+from src.commons.big_query.big_query_table_metadata import BigQueryTableMetadata
 from src.commons.table_reference import TableReference
+from src.retention.policy.fifo_10_up_to_7_months import Fifo10UpTo7Months
 from tests.utils.backup_utils import create_backup_daily_sequence, create_backup
 
 
@@ -24,38 +23,297 @@ class TestFifo10UpTo7Months(unittest.TestCase):
         patch.stopall()
 
     @freeze_time("2017-08-20")
-    def test_should_delete_only_backups_which_are_older_than_7_months_and_are_above_10th_version(self):  # nopep8 pylint: disable=C0301
+    def test_should_not_delete_if_single_young_backup(self):
         # given
         reference = TableReference('example-project-id', 'example-dataset-id',
                                    'example-table-id')
-        backups = create_backup_daily_sequence(20,
-                                               start_date=datetime(2016, 12, 1))
-        youngest_backup = create_backup(datetime(2017, 8, 1))
-        all_backups = list(backups + [youngest_backup])
+        single_young_backup = create_backup(datetime(2017, 8, 1))
+        all_backups = list([single_young_backup])
 
         # when
         eligible_for_deletion = self.under_test \
             .get_backups_eligible_for_deletion(backups=list(all_backups),
                                                table_reference=reference)
         # then
-        self.sortAndAssertListEqual(backups, eligible_for_deletion)
+        self.sortAndAssertListEqual([], eligible_for_deletion)
 
     @freeze_time("2017-08-20")
-    def test_should_delete_single_backup_which_is_older_than_7_months_and_is_11th_version(self):  # nopep8 pylint: disable=C0301
+    def test_should_not_delete_if_less_than_10_young_backups(self):
         # given
         reference = TableReference('example-project-id', 'example-dataset-id',
                                    'example-table-id')
-        backups = create_backup_daily_sequence(10,
-                                               start_date=datetime(2017, 6, 1))
-        oldest_backup = create_backup(datetime(2016, 12, 31))
-        backups.append(oldest_backup)
+        young_backups = create_backup_daily_sequence(3,
+                                                     start_date=datetime(2017, 8, 1))
+        # when
+        eligible_for_deletion = self.under_test \
+            .get_backups_eligible_for_deletion(backups=list(young_backups),
+                                               table_reference=reference)
+        # then
+        self.sortAndAssertListEqual([], eligible_for_deletion)
+
+    @freeze_time("2017-08-20")
+    def test_should_not_delete_if_10_young_backups(self):
+        # given
+        reference = TableReference('example-project-id', 'example-dataset-id',
+                                   'example-table-id')
+        young_backups = create_backup_daily_sequence(10,
+                                                     start_date=datetime(2017, 8, 1))
+        # when
+        eligible_for_deletion = self.under_test \
+            .get_backups_eligible_for_deletion(backups=list(young_backups),
+                                               table_reference=reference)
+        # then
+        self.sortAndAssertListEqual([], eligible_for_deletion)
+
+    @freeze_time("2017-08-20")
+    def test_should_delete_oldest_if_11_young_backups(self):
+        # given
+        reference = TableReference('example-project-id', 'example-dataset-id',
+                                   'example-table-id')
+        young_backups = create_backup_daily_sequence(11,
+                                                     start_date=datetime(2017, 8, 1))
+        # when
+        eligible_for_deletion = self.under_test \
+            .get_backups_eligible_for_deletion(backups=list(young_backups),
+                                               table_reference=reference)
+        # then
+        self.sortAndAssertListEqual([young_backups[10]], eligible_for_deletion)
+
+    @freeze_time("2017-08-20")
+    def test_should_delete_same_day_duplicates_backups(self):
+        #given
+        reference = TableReference('example-project-id', 'example-dataset-id',
+                                   'example-table-id')
+        first_5_backups = create_backup_daily_sequence(
+            5,
+            start_date=datetime(2017, 6, 1, 12)
+        )
+        second_5_backups = create_backup_daily_sequence(
+            5,
+            start_date=datetime(2017, 6, 6, 12)
+        )
+        first_5_backups_duplicated = create_backup_daily_sequence(
+            5,
+            start_date=datetime(2017, 6, 1, 14)
+        )
+
+        backups = list(
+            first_5_backups + second_5_backups + first_5_backups_duplicated
+        )
+        backups_expected_for_deletion = list(first_5_backups)
+
+        #when
+        eligible_for_deletion = \
+            self.under_test.get_backups_eligible_for_deletion(
+                backups=list(backups),
+                table_reference=reference)
+        #then
+        self.sortAndAssertListEqual(
+            backups_expected_for_deletion,
+            eligible_for_deletion
+        )
+
+    @patch.object(BigQueryTableMetadata, 'table_exists', return_value=True)
+    @patch.object(BigQueryTableMetadata, 'get_table_by_reference', return_value=BigQueryTableMetadata(None))
+    @patch.object(Backup, 'get_table', return_value=Table(last_checked=datetime(2017, 8, 19)))
+    @freeze_time("2017-08-20")
+    def test_should_not_delete_if_single_old_backup(self, _1,_2,_3):
+        # given
+        reference = TableReference('example-project-id', 'example-dataset-id',
+                                   'example-table-id')
+
+        single_old_backup = create_backup(datetime(2016, 8, 1))
 
         # when
         eligible_for_deletion = self.under_test \
-            .get_backups_eligible_for_deletion(backups=list(backups),
+            .get_backups_eligible_for_deletion(backups=list([single_old_backup]),
                                                table_reference=reference)
         # then
-        self.sortAndAssertListEqual([oldest_backup], eligible_for_deletion)
+        self.sortAndAssertListEqual([], eligible_for_deletion)
+
+    @patch.object(BigQueryTableMetadata, 'table_exists', return_value=True)
+    @patch.object(BigQueryTableMetadata, 'get_table_by_reference', return_value=BigQueryTableMetadata(None))
+    @patch.object(Backup, 'get_table', return_value=Table(last_checked=datetime(2017, 8, 19)))
+    @freeze_time("2017-08-20")
+    def test_should_delete_older_backup_if_two_old_backups(self, _1,_2,_3):
+        # given
+        reference = TableReference('example-project-id', 'example-dataset-id',
+                                   'example-table-id')
+        older_old_backup = create_backup(datetime(2016, 7, 31))
+        old_backup = create_backup(datetime(2016, 8, 1))
+        all_backups = list([older_old_backup, old_backup])
+
+        # when
+        eligible_for_deletion = self.under_test \
+            .get_backups_eligible_for_deletion(backups=all_backups,
+                                               table_reference=reference)
+        # then
+        self.sortAndAssertListEqual([older_old_backup], eligible_for_deletion)
+
+
+    @patch.object(BigQueryTableMetadata, 'table_exists', return_value=True)
+    @patch.object(BigQueryTableMetadata, 'get_table_by_reference', return_value=BigQueryTableMetadata(None))
+    @patch.object(Backup, 'get_table', return_value=Table(last_checked=datetime(2017, 8, 19)))
+    @freeze_time("2017-08-20")
+    def test_should_not_delete_if_single_young_backup_and_single_old_backup(self, _1,_2,_3):
+        # given
+        reference = TableReference('example-project-id', 'example-dataset-id',
+                                   'example-table-id')
+        young_backup = create_backup(datetime(2017, 8, 1))
+        old_backup = create_backup(datetime(2016, 8, 1))
+        all_backups = [young_backup, old_backup]
+
+        # when
+        eligible_for_deletion = self.under_test \
+            .get_backups_eligible_for_deletion(backups=list(all_backups),
+                                               table_reference=reference)
+        # then
+        self.sortAndAssertListEqual([], eligible_for_deletion)
+
+
+    @patch.object(BigQueryTableMetadata, 'table_exists', return_value=True)
+    @patch.object(BigQueryTableMetadata, 'get_table_by_reference', return_value=BigQueryTableMetadata(None))
+    @patch.object(Backup, 'get_table', return_value=Table(last_checked=datetime(2017, 8, 19)))
+    @freeze_time("2017-08-20")
+    def test_should_not_delete_if_less_than_10_young_backup_and_single_old_backup(self, _1,_2,_3):
+        # given
+        reference = TableReference('example-project-id', 'example-dataset-id',
+                                   'example-table-id')
+        young_backups = create_backup_daily_sequence(3,
+                                                     start_date=datetime(2017, 8, 1))
+        old_backup = create_backup(datetime(2016, 8, 1))
+        all_backups = list(young_backups + [old_backup])
+
+        # when
+        eligible_for_deletion = self.under_test \
+            .get_backups_eligible_for_deletion(backups=list(all_backups),
+                                               table_reference=reference)
+        # then
+        self.sortAndAssertListEqual([], eligible_for_deletion)
+
+    @patch.object(BigQueryTableMetadata, 'table_exists', return_value=True)
+    @patch.object(BigQueryTableMetadata, 'get_table_by_reference', return_value=BigQueryTableMetadata(None))
+    @patch.object(Backup, 'get_table', return_value=Table(last_checked=datetime(2017, 8, 19)))
+    @freeze_time("2017-08-20")
+    def test_should_not_delete_if_10_young_backup_and_single_old_backup(self, _1,_2,_3):
+        # given
+        reference = TableReference('example-project-id', 'example-dataset-id',
+                                   'example-table-id')
+        young_backups = create_backup_daily_sequence(10,
+                                                     start_date=datetime(2017, 8, 1))
+        old_backup = create_backup(datetime(2016, 8, 1))
+        all_backups = list(young_backups + [old_backup])
+
+        # when
+        eligible_for_deletion = self.under_test \
+            .get_backups_eligible_for_deletion(backups=list(all_backups),
+                                               table_reference=reference)
+        # then
+        self.sortAndAssertListEqual([], eligible_for_deletion)
+
+    @patch.object(BigQueryTableMetadata, 'table_exists', return_value=True)
+    @patch.object(BigQueryTableMetadata, 'get_table_by_reference', return_value=BigQueryTableMetadata(None))
+    @patch.object(Backup, 'get_table', return_value=Table(last_checked=datetime(2017, 8, 19)))
+    @freeze_time("2017-08-20")
+    def test_should_delete_oldest_young_backup_if_11_young_backup_and_single_old_backup(self, _1,_2,_3):
+        # given
+        reference = TableReference('example-project-id', 'example-dataset-id',
+                                   'example-table-id')
+        young_backups = create_backup_daily_sequence(11,
+                                                     start_date=datetime(2017, 8, 1))
+        old_backup = create_backup(datetime(2016, 8, 1))
+        all_backups = list(young_backups + [old_backup])
+
+        # when
+        eligible_for_deletion = self.under_test \
+            .get_backups_eligible_for_deletion(backups=list(all_backups),
+                                               table_reference=reference)
+        # then
+        self.sortAndAssertListEqual([young_backups[10]], eligible_for_deletion)
+
+    @patch.object(BigQueryTableMetadata, 'table_exists', return_value=True)
+    @patch.object(BigQueryTableMetadata, 'get_table_by_reference', return_value=BigQueryTableMetadata(None))
+    @patch.object(Backup, 'get_table', return_value=Table(last_checked=datetime(2017, 8, 19)))
+    @freeze_time("2017-08-20")
+    def test_should_delete_older_old_backup_if_two_old_and_single_young_backups(self, _1,_2,_3):
+        # given
+        reference = TableReference('example-project-id', 'example-dataset-id',
+                                   'example-table-id')
+        older_old_backup = create_backup(datetime(2016, 7, 31))
+        old_backup = create_backup(datetime(2016, 8, 1))
+        young_backup = create_backup(datetime(2017, 8, 1))
+        all_backups = list([older_old_backup, old_backup, young_backup])
+
+        # when
+        eligible_for_deletion = self.under_test \
+            .get_backups_eligible_for_deletion(backups=all_backups,
+                                               table_reference=reference)
+        # then
+        self.sortAndAssertListEqual([older_old_backup], eligible_for_deletion)
+
+    @patch.object(BigQueryTableMetadata, 'table_exists', return_value=True)
+    @patch.object(BigQueryTableMetadata, 'get_table_by_reference', return_value=BigQueryTableMetadata(None))
+    @patch.object(Backup, 'get_table', return_value=Table(last_checked=datetime(2017, 8, 19)))
+    @freeze_time("2017-08-20")
+    def test_should_delete_older_old_backup_if_two_old_and_less_than_10_young_backups(self, _1,_2,_3):
+        # given
+        reference = TableReference('example-project-id', 'example-dataset-id',
+                                   'example-table-id')
+        older_old_backup = create_backup(datetime(2016, 7, 31))
+        old_backup = create_backup(datetime(2016, 8, 1))
+        young_backups = create_backup_daily_sequence(3,
+                                                     start_date=datetime(2017, 8, 1))
+        all_backups = list(young_backups + [older_old_backup, old_backup])
+
+        # when
+        eligible_for_deletion = self.under_test \
+            .get_backups_eligible_for_deletion(backups=all_backups,
+                                               table_reference=reference)
+        # then
+        self.sortAndAssertListEqual([older_old_backup], eligible_for_deletion)
+
+    @patch.object(BigQueryTableMetadata, 'table_exists', return_value=True)
+    @patch.object(BigQueryTableMetadata, 'get_table_by_reference', return_value=BigQueryTableMetadata(None))
+    @patch.object(Backup, 'get_table', return_value=Table(last_checked=datetime(2017, 8, 19)))
+    @freeze_time("2017-08-20")
+    def test_should_delete_older_old_backup_if_two_old_and_10_young_backups(self, _1,_2,_3):
+        # given
+        reference = TableReference('example-project-id', 'example-dataset-id',
+                                   'example-table-id')
+        older_old_backup = create_backup(datetime(2016, 7, 31))
+        old_backup = create_backup(datetime(2016, 8, 1))
+        young_backups = create_backup_daily_sequence(10,
+                                                     start_date=datetime(2017, 8, 1))
+        all_backups = list(young_backups + [older_old_backup, old_backup])
+
+        # when
+        eligible_for_deletion = self.under_test \
+            .get_backups_eligible_for_deletion(backups=all_backups,
+                                               table_reference=reference)
+        # then
+        self.sortAndAssertListEqual([older_old_backup], eligible_for_deletion)
+
+    @patch.object(BigQueryTableMetadata, 'table_exists', return_value=True)
+    @patch.object(BigQueryTableMetadata, 'get_table_by_reference', return_value=BigQueryTableMetadata(None))
+    @patch.object(Backup, 'get_table', return_value=Table(last_checked=datetime(2017, 8, 19)))
+    @freeze_time("2017-08-20")
+    def test_should_delete_older_old_and_oldest_young_backup_if_two_old_and_11_young_backups(self, _1,_2,_3):
+        # given
+        reference = TableReference('example-project-id', 'example-dataset-id',
+                                   'example-table-id')
+        older_old_backup = create_backup(datetime(2016, 7, 31))
+        old_backup = create_backup(datetime(2016, 8, 1))
+        young_backups = create_backup_daily_sequence(11,
+                                                     start_date=datetime(2017, 8, 1))
+        all_backups = list(young_backups + [older_old_backup,
+                                            old_backup])
+
+        # when
+        eligible_for_deletion = self.under_test \
+            .get_backups_eligible_for_deletion(backups=all_backups,
+                                               table_reference=reference)
+        # then
+        self.sortAndAssertListEqual([older_old_backup, young_backups[10]], eligible_for_deletion)
 
     @patch.object(BigQueryTableMetadata, 'table_exists', return_value=True)
     @patch.object(BigQueryTableMetadata, 'get_table_by_reference', return_value=BigQueryTableMetadata(None))
@@ -104,7 +362,7 @@ class TestFifo10UpTo7Months(unittest.TestCase):
         )
 
     @freeze_time("2017-08-20")
-    def test_should_remove_above_last_10_backups(self):
+    def test_should_remove_above_last_10_young_backups(self):
         #given
         reference = TableReference(
             'example-project-id',
@@ -121,41 +379,6 @@ class TestFifo10UpTo7Months(unittest.TestCase):
         backups.append(oldest_backup)
 
         backups_expected_for_deletion = [oldest_backup]
-
-        #when
-        eligible_for_deletion = \
-            self.under_test.get_backups_eligible_for_deletion(
-                backups=list(backups),
-                table_reference=reference)
-        #then
-        self.sortAndAssertListEqual(
-            backups_expected_for_deletion,
-            eligible_for_deletion
-        )
-
-
-    @freeze_time("2017-08-20")
-    def test_should_first_remove_same_day_duplicates_backups(self):
-        #given
-        reference = TableReference('example-project-id', 'example-dataset-id',
-                                   'example-table-id')
-        first_5_backups = create_backup_daily_sequence(
-            5,
-            start_date=datetime(2017, 6, 1, 12)
-        )
-        second_5_backups = create_backup_daily_sequence(
-            5,
-            start_date=datetime(2017, 6, 6, 12)
-        )
-        first_5_backups_duplicated = create_backup_daily_sequence(
-            5,
-            start_date=datetime(2017, 6, 1, 14)
-        )
-
-        backups = list(
-            first_5_backups + second_5_backups + first_5_backups_duplicated
-        )
-        backups_expected_for_deletion = list(first_5_backups)
 
         #when
         eligible_for_deletion = \
--- tests/retention/test_table_retention.py
+++ tests/retention/test_table_retention.py
@@ -3,16 +3,13 @@ import unittest
 
 from google.appengine.ext import ndb
 from google.appengine.ext import testbed
-from mock import patch, call, PropertyMock
+from mock import patch, Mock
 
-from src.commons.config.configuration import Configuration
 from src.backup.datastore.Backup import Backup
 from src.backup.datastore.Table import Table
 from src.commons.big_query.big_query import BigQuery, TableNotFoundException
-from src.retention.policy.delete_non_partitioned_tables_above_10_versions import \
-    DeleteNonPartitionedTablesOlderThan10Versions
-from src.retention.table_retention import TableRetention
 from src.commons.table_reference import TableReference
+from src.retention.table_retention import TableRetention
 from tests.utils import backup_utils
 
 
@@ -21,8 +18,8 @@ class TestTableRetention(unittest.TestCase):
     def setUp(self):
         patch('googleapiclient.discovery.build').start()
         self.init_testbed_for_datastore()
-        self.under_test = TableRetention(
-            DeleteNonPartitionedTablesOlderThan10Versions())
+        self.policy = Mock()
+        self.under_test = TableRetention(self.policy)
 
     def init_testbed_for_datastore(self):
         self.testbed = testbed.Testbed()
@@ -35,10 +32,8 @@ class TestTableRetention(unittest.TestCase):
         self.testbed.deactivate()
         patch.stopall()
 
-    @patch.object(BigQuery, 'delete_table', side_effect=[TableNotFoundException('Table Not Found'), None])  # nopep8 pylint: disable=C0301
-    @patch("src.retention.policy.delete_non_partitioned_tables_above_10_versions.DeleteNonPartitionedTablesOlderThan10Versions.NUMBER_OF_BACKUPS_TO_KEEP", 0) # nopep8 pylint: disable=C0301
-    @patch.object(Configuration, 'backup_project_id', return_value='dummy_project_id', new_callable=PropertyMock)
-    def test_that_None_in_deleted_attribute_should_be_updated_with_a_date_when_table_not_found(self, _, _1): # nopep8 pylint: disable=C0301,W0613
+    @patch.object(BigQuery, 'delete_table')
+    def test_should_fill_deleted_field_in_backup_entity_after_deletion(self, _):
         # given
         table = Table(
             project_id='example-proj-name',
@@ -47,26 +42,26 @@ class TestTableRetention(unittest.TestCase):
             last_checked=datetime.datetime(2017, 2, 1, 16, 30)
         )
         table.put()
+        reference = TableReference.from_table_entity(table)
         backup1 = backup_utils.create_backup(
             datetime.datetime(2017, 2, 1, 16, 30), table, table_id="backup1")
         backup2 = backup_utils.create_backup(
             datetime.datetime(2017, 2, 2, 16, 30), table, table_id="backup2")
         ndb.put_multi([backup1, backup2])
-
-        reference = TableReference.from_table_entity(table)
+        self.policy.get_backups_eligible_for_deletion = Mock(
+            return_value=[backup1, backup2])
 
         # when
         self.under_test.perform_retention(reference, table.key.urlsafe())
+
         # then
         self.assertTrue(
             Backup.get_by_key(backup1.key).deleted is not None)
         self.assertTrue(
             Backup.get_by_key(backup2.key).deleted is not None)
 
-    @patch.object(BigQuery, 'delete_table', side_effect=[TableNotFoundException('Table Not Found'), None]) # nopep8 pylint: disable=C0301
-    @patch("src.retention.policy.delete_non_partitioned_tables_above_10_versions.DeleteNonPartitionedTablesOlderThan10Versions.NUMBER_OF_BACKUPS_TO_KEEP", 0) # nopep8 pylint: disable=C0301
-    @patch.object(Configuration, 'backup_project_id', return_value='dummy_project_id', new_callable=PropertyMock)
-    def test_that_if_first_backup_of_a_table_not_found_the_next_one_will_proceed(self, _, delete_table): # nopep8 pylint: disable=C0301, W0613, R0201
+    @patch.object(BigQuery, 'delete_table')
+    def test_should_not_delete_any_table_if_policy_return_empty_list(self, delete_table):
         # given
         table = Table(
             project_id='example-proj-name',
@@ -75,17 +70,97 @@ class TestTableRetention(unittest.TestCase):
             last_checked=datetime.datetime(2017, 2, 1, 16, 30)
         )
         table.put()
+        reference = TableReference.from_table_entity(table)
         backup1 = backup_utils.create_backup(
             datetime.datetime(2017, 2, 1, 16, 30), table, table_id="backup1")
         backup2 = backup_utils.create_backup(
             datetime.datetime(2017, 2, 2, 16, 30), table, table_id="backup2")
         ndb.put_multi([backup1, backup2])
+        self.policy.get_backups_eligible_for_deletion = Mock(return_value=[])
+
+        # when
+        self.under_test.perform_retention(reference, table.key.urlsafe())
+
+        # then
+        self.assertTrue(
+            Backup.get_by_key(backup1.key).deleted is None)
+        self.assertTrue(
+            Backup.get_by_key(backup2.key).deleted is None)
+        delete_table.assert_not_called()
+
+    @patch.object(BigQuery, 'delete_table',
+                  side_effect=[TableNotFoundException('Table Not Found'), None])
+    def test_should_fill_deleted_field_in_backup_entity_if_table_not_found_error_during_deletion(
+            self, _):
+        # given
+        table = Table(
+            project_id='example-proj-name',
+            dataset_id='example-dataset-name',
+            table_id='example-table-name',
+            last_checked=datetime.datetime(2017, 2, 1, 16, 30)
+        )
+        table.put()
+        reference = TableReference.from_table_entity(table)
+        backup1 = backup_utils.create_backup(
+            datetime.datetime(2017, 2, 1, 16, 30), table, table_id="backup1")
+        backup2 = backup_utils.create_backup(
+            datetime.datetime(2017, 2, 2, 16, 30), table, table_id="backup2")
+        ndb.put_multi([backup1, backup2])
+        self.policy.get_backups_eligible_for_deletion = Mock(
+            return_value=[backup1, backup2])
+
+        # when
+        self.under_test.perform_retention(reference, table.key.urlsafe())
+
+        # then
+        self.assertTrue(
+            Backup.get_by_key(backup1.key).deleted is not None)
+        self.assertTrue(
+            Backup.get_by_key(backup2.key).deleted is not None)
+
+    @patch.object(BigQuery, 'delete_table')
+    def test_should_fill_deleted_field_in_backup_entity_only_for_deleted_backup(
+            self, _):
+        # given
+        table = Table(
+            project_id='example-proj-name',
+            dataset_id='example-dataset-name',
+            table_id='example-table-name',
+            last_checked=datetime.datetime(2017, 2, 1, 16, 30)
+        )
+        table.put()
+        reference = TableReference.from_table_entity(table)
+        backup1 = backup_utils.create_backup(
+            datetime.datetime(2017, 2, 1, 16, 30), table, table_id="backup1")
+        backup2 = backup_utils.create_backup(
+            datetime.datetime(2017, 2, 2, 16, 30), table, table_id="backup2")
+        ndb.put_multi([backup1, backup2])
+        self.policy.get_backups_eligible_for_deletion = Mock(
+            return_value=[backup1])
+
+        # when
+        self.under_test.perform_retention(reference, table.key.urlsafe())
+
+        # then
+        self.assertTrue(
+            Backup.get_by_key(backup1.key).deleted is not None)
+        self.assertTrue(
+            Backup.get_by_key(backup2.key).deleted is None)
+
+    @patch.object(BigQuery, 'delete_table')
+    def test_should_not_perform_retention_if_no_backups(self, delete_table):
+        # given
+        table = Table(
+            project_id='example-proj-name',
+            dataset_id='example-dataset-name',
+            table_id='example-table-name',
+            last_checked=datetime.datetime(2017, 2, 1, 16, 30)
+        )
+        table.put()
         reference = TableReference.from_table_entity(table)
 
         # when
         self.under_test.perform_retention(reference, table.key.urlsafe())
+
         # then
-        delete_table.assert_has_calls([
-            call(TableReference('dummy_project_id', 'targetDataset', 'backup2')),
-            call(TableReference('dummy_project_id', 'targetDataset', 'backup1'))
-        ])
+        delete_table.assert_not_called()
