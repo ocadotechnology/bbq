2a35221eec4e72bbded4eddb9bd26b81ab749562
Przemyslaw Jasinski <p.jasinski@ocado.com>
2019-05-24 16:04:21 +0200

    YACHT-1002: Quick fix in terraform

--- terraform/sli/datastore_export_dataset.tf
+++ terraform/sli/datastore_export_dataset.tf
@@ -22,13 +22,13 @@ resource "google_bigquery_dataset" "datastore_export_dataset" {
 resource "google_bigquery_table" "datastore_export_backup_kind_table" {
   project = "${local.datastore_export_project}"
   dataset_id = "${google_bigquery_dataset.datastore_export_dataset.dataset_id}"
-  table_id = "Backup_"
+  table_id = "Backup_19700101"
   schema= "${file("${path.module}/datastore_export_backup_kind_table_schema.json")}"
 }
 
 resource "google_bigquery_table" "datastore_export_table_kind_table" {
   project = "${local.datastore_export_project}"
   dataset_id = "${google_bigquery_dataset.datastore_export_dataset.dataset_id}"
-  table_id = "Table_"
+  table_id = "Table_19700101"
   schema= "${file("${path.module}/datastore_export_table_kind_table_schema.json")}"
 }
