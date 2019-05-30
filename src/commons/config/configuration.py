e03de21292b9df7cf3dcf0850659df1e82acf10f
Mateusz Radko <mateusz.radko@ocado.com>
2019-05-30 16:54:44 +0200

    NON-JIRA: Better config property description. Validation for retention properties (number of months >= 1).

--- src/commons/config/configuration.py
+++ src/commons/config/configuration.py
@@ -52,11 +52,16 @@ class Configuration(object):
 
     @property
     def grace_period_after_source_table_deletion_in_months(self):
-        return self.__project_config['retention_settings'].get('grace_period_after_source_table_deletion_in_months')    \
+        property = self.__project_config['retention_settings'].get(
+            'grace_period_after_source_table_deletion_in_months')
+        assert property >= 1
+        return property
 
     @property
     def young_old_generation_threshold_in_months(self):
-        return self.__project_config['retention_settings'].get('young_old_generation_threshold_in_months')
+        property = self.__project_config['retention_settings'].get('young_old_generation_threshold_in_months')
+        assert property >= 1
+        return property
 
 config_file_yaml = "config/config.yaml"
 logging.info("Loading configuration from file: '%s'", config_file_yaml)
