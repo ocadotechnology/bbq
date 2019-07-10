c2e3eb7c213c6e46968b17110b88e01d967dc20c
Mateusz Radko <mateusz.radko@ocado.com>
2019-07-10 17:48:49 +0200

    Enforcing empty list of members having Editor IAM role on bbq project.

--- terraform/bbq/project_iams.tf
+++ terraform/bbq/project_iams.tf
@@ -1,11 +1,9 @@
-// Uncomment after fixing https://github.com/terraform-providers/terraform-provider-google/issues/2007
-//resource "google_project_iam_binding" "project_editor_iams" {
-//  project = "${var.bbq_project}"
-//  role    = "roles/editor"
-//
-//  members = [
-//  ]
-//}
+resource "google_project_iam_binding" "project_editor_iams" {
+  project = var.bbq_project
+  role = "roles/editor"
+
+  members = [] // no editors!
+}
 
 resource "google_project_iam_member" "project_indexadmin_iam" {
   project = var.bbq_project
