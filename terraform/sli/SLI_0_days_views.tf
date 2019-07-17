653de1edacf6c59afdc35bc86294b313d268bda3
Mateusz Radko <mateusz.radko@ocado.com>
2019-07-17 15:18:34 +0200

    YACHT-1176: Updating Census views from the past to use newer snapshots with fresh data. It should minimise delay of displaying table as violation in most pesimistic case.

--- terraform/sli/SLI_0_days_views.tf
+++ terraform/sli/SLI_0_days_views.tf
@@ -17,7 +17,8 @@ resource "google_bigquery_table" "census_data_0_days_ago_view" {
                 FROM
                   [${var.gcp_census_project}.bigquery.table_metadata_v1_0]
                 WHERE
-                  _PARTITIONTIME BETWEEN TIMESTAMP(DATE_ADD(CURRENT_DATE(), -3, "DAY")) AND TIMESTAMP(CURRENT_DATE())
+                  _PARTITIONTIME BETWEEN TIMESTAMP(DATE_ADD(NOW(), -3, "DAY")) AND NOW()
+                  AND lastModifiedTime <= NOW()
                   AND timePartitioning.type IS NULL AND type='TABLE'
               )
               WHERE rownum = 1
@@ -30,7 +31,8 @@ resource "google_bigquery_table" "census_data_0_days_ago_view" {
                   FROM
                     [${var.gcp_census_project}.bigquery.partition_metadata_v1_0]
                   WHERE
-                    _PARTITIONTIME BETWEEN TIMESTAMP(DATE_ADD(CURRENT_DATE(), -3, "DAY")) AND TIMESTAMP(CURRENT_DATE())
+                  _PARTITIONTIME BETWEEN TIMESTAMP(DATE_ADD(NOW(), -3, "DAY")) AND NOW()
+                  AND lastModifiedTime <= NOW()
               )
               WHERE rownum = 1
             )
