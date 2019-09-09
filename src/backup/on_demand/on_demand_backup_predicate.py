a1332b0341b5d20a7680873c9769c8575f810276
Paweł Kmita <pawel.kmita@ocado.com>
2019-09-09 16:25:52 +0200

    YACHT-1259 Add exception handling for on-demand table backup, code refactor, test refactor

--- src/backup/on_demand/on_demand_backup_predicate.py
+++ src/backup/on_demand/on_demand_backup_predicate.py
@@ -2,17 +2,22 @@ import logging
 
 from src.backup.abstract_backup_predicate import \
     AbstractBackupPredicate
+from src.commons.exceptions import ParameterValidationException, NotFoundException
 
 
 class OnDemandBackupPredicate(AbstractBackupPredicate):
 
     def test(self, big_query_table_metadata, table_entity):
-        if not self._is_possible_to_copy_table(big_query_table_metadata):
-            return False
+        table_validation_status, table_validation_message = self._is_possible_to_copy_table(big_query_table_metadata)
+        if not table_validation_status:
+            if table_validation_message == "Table not found":
+                raise NotFoundException(table_validation_message)
+            else:
+                raise ParameterValidationException(table_validation_message)
 
         logging.info("Performing on-demand backup for %s."
                      "It is performed without checking "
-                     "if table aready has up to date backup",
+                     "if table already has up to date backup",
                      big_query_table_metadata.table_reference())
 
         return True
